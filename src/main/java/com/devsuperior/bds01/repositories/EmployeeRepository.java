2ed48803033bf905a8f8d8e2bb643eef97195a17
Vitor Battestin <vitor.battestin@gmail.com>
2021-06-01 17:16:43 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/repositories/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.bds01.repositories;
+
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
